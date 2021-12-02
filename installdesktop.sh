cfcc3dc936b9e28578dcf14d1ddd5a4af65117ae
NotteShock <85934291+NotteShock@users.noreply.github.com>
2021-12-02 08:30:04 +0700

    Update installdesktop.sh

--- installdesktop.sh
+++ installdesktop.sh
@@ -1,2 +1,1 @@
-echo Installing MaterialTermuxDesktop...
-apt install unstable-repo x11-repo xfce4 xfce4-goodies tigervnc xorgproto
+echo Installing MaterialTermuxDesktop... && apt install unstable-repo x11-repo && apt install xfce4 xfce4-goodies tigervnc xorgproto
