98277cdfaee333618eb2eab88dfc7574c89663ce
NotteShock <85934291+NotteShock@users.noreply.github.com>
2021-12-02 19:51:48 +0700

    Update installdesktop.sh

--- installdesktop.sh
+++ installdesktop.sh
@@ -5,7 +5,8 @@ ENDCOLOR="\e[0m"
 
 cd $HOME && clear && read -p "Do you want to install the desktop (y/n)?" choice
 case "$choice" in 
-  y|Y ) echo "";;
+  y|Y ) echo "You choose to install the desktop
+";;
   n|N ) echo "Action refused by user";;
   * ) echo "Invalid answer";;
 esac
