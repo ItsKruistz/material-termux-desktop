44453492beda18e6ba6fc866d5997ddedf878915
NotteShock <85934291+NotteShock@users.noreply.github.com>
2021-12-02 20:10:04 +0700

    Update installdesktop.sh

--- installdesktop.sh
+++ installdesktop.sh
@@ -12,7 +12,12 @@ echo -e "${GREEN}
 (•) Installing the desktop...
 ${ENDCOLOR}"
 apt install unstable-repo x11-repo
-apt install termux-api python xfce4-terminal lxqt-session thunar lxqt-panel openbox lxqt-config obconf tigervnc gl4es papirus-icon-theme mpc mpd texlive-roboto unzip zip bsdtar otter-browser geany audacious kvantum geany neofetch xorg-xhost gtk2-engines-murrine picom sl -y && tar -xf themes.tar.gz -C $PREFIX/share/themes/ && cd && wget https://raw.githubusercontent.com/NotteShock/material-termux-desktop/main/startdesktop.sh && chmod +x *.sh && cd $HOME
+apt install termux-api python xfce4-terminal lxqt-session thunar lxqt-panel openbox lxqt-config obconf tigervnc gl4es papirus-icon-theme mpc mpd texlive-roboto unzip zip bsdtar otter-browser geany audacious kvantum geany neofetch xorg-xhost gtk2-engines-murrine picom sl -y
+tar -xf themes.tar.gz -C $PREFIX/share/themes/
+cd
+wget https://raw.githubusercontent.com/NotteShock/material-termux-desktop/main/startdesktop.sh
+chmod +x *.sh
+cd $HOME
 
 elif [[ $REPLY =~ ^[Nn]$ ]]
 then
