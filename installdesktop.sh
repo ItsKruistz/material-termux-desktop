c9e3d2e85fab70cd2702c79bfb7bb05b286aab16
NotteShock <85934291+NotteShock@users.noreply.github.com>
2021-12-02 13:07:31 +0700

    Update installdesktop.sh

--- installdesktop.sh
+++ installdesktop.sh
@@ -1,6 +1,6 @@
 #! $PREFIX/bin/env bash
 
-GREEN="\e[32m"to
+GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
-clear && cd $HOME && echo -e "${GREEN}And this is some green text${ENDCOLOR}" && apt install unstable-repo x11-repo && apt install termux-api python xfce4 xfce4-whiskermenu-plugin tigervnc gtk3 gl4es papirus-icon-theme mpc mpd unzip zip bsdtar otter-browser xfce4-terminal xarchiver geany audacious kvantum icecast libaom libde265 libtheora libxxf86vm geany neofetch macchina screenfetch gtk2-engines-murrine sl -y && cd && wget https://raw.githubusercontent.com/NotteShock/material-termux-desktop/main/startdesktop.sh && chmod +x *.sh && cd $HOME && vncserver -kill :1 && ./startdesktop.sh
+clear && cd $HOME && echo -e "${GREEN}Installing the desktop...${ENDCOLOR}" && apt install unstable-repo x11-repo && apt install termux-api python xfce4 xfce4-whiskermenu-plugin tigervnc gtk3 gl4es papirus-icon-theme mpc mpd unzip zip bsdtar otter-browser xfce4-terminal xarchiver geany audacious kvantum icecast libaom libde265 libtheora libxxf86vm geany neofetch macchina screenfetch gtk2-engines-murrine sl -y && cd && wget https://raw.githubusercontent.com/NotteShock/material-termux-desktop/main/startdesktop.sh && chmod +x *.sh && cd $HOME && vncserver -kill :1 && ./startdesktop.sh
