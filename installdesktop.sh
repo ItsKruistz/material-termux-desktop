0ad9d40d1976edaa36fcef5889de26ef24d07528
NotteShock <85934291+NotteShock@users.noreply.github.com>
2021-12-03 06:20:49 +0700

    Update installdesktop.sh

--- installdesktop.sh
+++ installdesktop.sh
@@ -13,7 +13,7 @@ echo -e "${GREEN}
 (•) Installing the desktop...
 ${ENDCOLOR}"
 apt install unstable-repo x11-repo
-apt install xcompmgr xfce4-terminal xfce4-panel xfce4-session xfce4-panel xfconf xfce4-settings-manager openbox xorg-xprop xorg-xhost neofetch texlive-roboto -y
+apt install xcompmgr audacious xpdf qt5-qtbase-gtk-platformtheme qt5-qttools qt5-qtx11extras lxqt lxqt-build-tools otter-browser qgit featherpad gtk2 gtk3 python-tkinter tigervnc xorg-xhost openbox geany qt5-qtwebsockets qt5-qtxmlpatterns qt5-qtdeclarative termux-api geany-plugins xorg-xprop neofetch sl texlive-roboto galculator qt5-qttools glade feathernotes xorg-xprop mtpaint xorg-xhost -y
 tar -xf themes.tar.gz -C $PREFIX/share/themes/
 cd
 wget https://raw.githubusercontent.com/NotteShock/material-termux-desktop/main/startdesktop.sh
