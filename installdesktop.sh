19cceb79680cd872e4ee16de04756781f9fb9035
NotteShock <85934291+NotteShock@users.noreply.github.com>
2021-12-02 08:50:30 +0700

    Update installdesktop.sh

--- installdesktop.sh
+++ installdesktop.sh
@@ -1,1 +1,1 @@
-echo Installing MaterialTermuxDesktop... && apt install unstable-repo x11-repo && apt install xfce4 xfce4-whiskermenu-plugin tigervnc gtk3 -y
+echo Installing MaterialTermuxDesktop... && apt install unstable-repo x11-repo && apt install xfce4 xfce4-whiskermenu-plugin tigervnc gtk3 papirus-icon-theme -y
