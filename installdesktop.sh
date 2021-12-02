6892b65556782d0f0ecab624236a2f90c7d3e847
NotteShock <85934291+NotteShock@users.noreply.github.com>
2021-12-02 17:22:40 +0700

    Update installdesktop.sh

--- installdesktop.sh
+++ installdesktop.sh
@@ -5,4 +5,4 @@ ENDCOLOR="\e[0m"
 
 && cd $HOME && clear && echo -e "${GREEN}
 (•) Installing the desktop...
-${ENDCOLOR}" && apt install unstable-repo x11-repo && apt install termux-api python xfce4-terminal lxqt-session thunar lxqt-panel openbox lxqt-config obconf tigervnc gl4es papirus-icon-theme mpc mpd texlive-roboto unzip zip bsdtar otter-browser geany audacious kvantum geany neofetch xorg-xhost gtk2-engines-murrine sl -y && cd && wget https://raw.githubusercontent.com/NotteShock/material-termux-desktop/main/startdesktop.sh && chmod +x *.sh && cd $HOME && vncserver -kill :1 && ./startdesktop.sh
+${ENDCOLOR}" && apt install unstable-repo x11-repo && apt install termux-api python xfce4-terminal lxqt-session thunar lxqt-panel openbox lxqt-config obconf tigervnc gl4es papirus-icon-theme mpc mpd texlive-roboto unzip zip bsdtar otter-browser geany audacious kvantum geany neofetch xorg-xhost gtk2-engines-murrine picom sl -y && cd && wget https://raw.githubusercontent.com/NotteShock/material-termux-desktop/main/startdesktop.sh && chmod +x *.sh && cd $HOME && vncserver -kill :1 && ./startdesktop.sh
