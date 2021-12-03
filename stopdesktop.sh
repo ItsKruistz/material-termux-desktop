d02c72263eecbfea7e2397ff2fec1b7fa61b5adf
NotteShock <85934291+NotteShock@users.noreply.github.com>
2021-12-03 07:43:39 +0700

    Update stopdesktop.sh

--- stopdesktop.sh
+++ stopdesktop.sh
@@ -6,3 +6,4 @@ rm -rf $HOME/.vnc/localhost:1.pid
 rm -rf $PREFIX/tmp/.X1-lock
 rm -rf $PREFIX/tmp/.X11-unix/X1
 echo "Desktop has been stopped successfully!"
+echo -e "${RED}Desktop has been stopped successfully!${ENDCOLOR}"
