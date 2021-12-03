9fff4a93c2f82bb952d47753dcd209261dc130c8
NotteShock <85934291+NotteShock@users.noreply.github.com>
2021-12-03 08:34:12 +0700

    Update startdesktop.sh

--- startdesktop.sh
+++ startdesktop.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-vncserver -geometry 1280x720 -xstartup $PREFIX/bin/startlxqt -listen tcp :1
-vncserver -listen tcp 
+vncserver -geometry 1280x720 -xstartup $PREFIX/bin/startlxqt -listen tcp
 vncserver -list
