449e1ebdab44932568778594535dd3bb9fe33ed8
NotteShock <85934291+NotteShock@users.noreply.github.com>
2021-12-03 07:25:39 +0700

    Update stopdesktop.sh

--- stopdesktop.sh
+++ stopdesktop.sh
@@ -2,5 +2,5 @@ killall Xvnc
 rm -rf $HOME/.vnc/localhost:1.pid
 rm -rf $PREFIX/tmp/.X1-lock
 rm -rf $PREFIX/tmp/.X11-unix/X1
-echo "stop desktop successfull" 
+echo "Desktop has been stopped successfully!" 
 
