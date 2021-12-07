3a5feec284c6b33ba0305bc746d37772a1ec0adf
thanksandnubbo <85934291+thanksandnubbo@users.noreply.github.com>
2021-12-07 19:05:27 +0700

    Update installdesktop.sh

--- installdesktop.sh
+++ installdesktop.sh
@@ -30,7 +30,7 @@ mkdir $HOME/Public
 mkdir $HOME/Documents
 mkdir $HOME/Pictures
 mkdir $HOME/Videos
-sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh'>>~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
