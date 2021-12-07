e03ef06d94ca26462ceaee5ff6383ad1a3ccef22
thanksandnubbo <85934291+thanksandnubbo@users.noreply.github.com>
2021-12-07 10:43:58 +0700

    Update startdesktop.sh

--- startdesktop.sh
+++ startdesktop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./stopdesktop.sh
-vncserver -geometry 1280x720 -xstartup $PREFIX/bin/startlxqt -listen tcp
+vncserver -geometry 1280x720 -xstartup $PREFIX/bin/startxfce4 -listen tcp
 vncserver -list
